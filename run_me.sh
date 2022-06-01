35270f45befa489261c82d8c1fcb91722a124bbb
regenworm <a.khawalid@gmail.com>
2022-06-01 10:37:56 +0200

    baseline config

--- run_me.sh
+++ run_me.sh
@@ -1,17 +1,19 @@
-DATA_PATH='data/toy64_split_0.8.json'
-# DATA_PATH='data/who_is_it.json'
+# DATA_PATH='data/toy64_split_0.8.json'
+DATA_PATH='data/who_is_it.json'
 VISIBLE_CUDA_DEVICES=0
 
-# Train the agents.
-python train.py -learningRate 0.01 -hiddenSize 100 -batchSize 1000 \
-                -imgFeatSize 20 -embedSize 20\
-                -dataset $DATA_PATH\
-                -aOutVocab 4 -qOutVocab 3
-
-# python train.py -learningRate 0.01 -hiddenSize 128 -batchSize 256 \
-#                 -imgFeatSize 16 -embedSize 16 -numEpochs 25000\
+# # Train the agents.
+# python train.py -learningRate 0.01 -hiddenSize 100 -batchSize 1000 \
+#                 -imgFeatSize 20 -embedSize 20  -numEpochs 25000\
 #                 -dataset $DATA_PATH\
-#                 -aOutVocab 4 -qOutVocab 2
+#                 -aOutVocab 2 -qOutVocab 17
+
+
+# baseline
+python train.py -learningRate 0.01 -hiddenSize 128 -batchSize 16 \
+                -imgFeatSize 16 -embedSize 16 -numEpochs 25000\
+                -dataset $DATA_PATH\
+                -aOutVocab 2 -qOutVocab 17 -numRounds 17
 
 
 # Test the agents (from a checkpoint) and visualize the dialogs.
